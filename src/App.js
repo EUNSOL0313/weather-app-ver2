45732f98c2b9b9d85ace3a3250d220d803ef6506
EUNSOL0313 <liat0313@gmail.com>
2024-03-26 21:36:50 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
    }
 
    const getCurrentLocationWeather = async (lat, lon) => {
-      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
       setLoading(true)
       let response = await fetch(url)
       let data = await response.json()
@@ -32,7 +32,7 @@ function App() {
    }
 
    const getWeatherByCity = async () => {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       setLoading(true)
       let response = await fetch(url)
       let data = await response.json()
